d057558ae6c1011d936332a2c983c19f699c4135
camenduru <camenduru@gmail.com>
2024-06-01 04:38:20 +0000

    first commit

---
+++ worker.py
@@ -0,0 +1,59 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+import json
+
+pipe = AutoPipelineForText2Image.from_pretrained(
+    "/content/model",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    requires_safety_checker=False).to("cuda:0")
+
+import gradio as gr
+
+def closestNumber(n, m):
+    q = int(n / m)
+    n1 = m * q
+    if (n * m) > 0:
+        n2 = m * (q + 1)
+    else:
+        n2 = m * (q - 1)
+    if abs(n - n1) < abs(n - n2):
+        return n1
+    return n2
+
+@torch.inference_mode()
+def generate(command):
+    values = json.loads(command)
+    width = closestNumber(values['width'], 8)
+    height = closestNumber(values['height'], 8)
+    image = pipe(values['prompt'], negative_prompt=values['negative_prompt'], num_inference_steps=25, guidance_scale=7.5, width=width, height=height).images[0]
+    image.save('/content/image.jpg')
+    return image
+
+with gr.Blocks(title=f"sdxl-turbo", css=".gradio-container {max-width: 544px !important}", analytics_enabled=False) as demo:
+    with gr.Row():
+      with gr.Column():
+          textbox = gr.Textbox(show_label=False, value="""
+            {   "prompt": "Totoro at the pool eating toast",
+                "negative_prompt": "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
+                "width": 1152,
+                "height": 832
+            }
+          """)
+          button = gr.Button()
+    with gr.Row(variant="default"):
+        output_image = gr.Image(
+            show_label=False,
+            format=".png",
+            type="pil",
+            interactive=False,
+            height=512,
+            width=512,
+            elem_id="output_image",
+        )
+
+    button.click(fn=generate, inputs=[textbox], outputs=[output_image], show_progress=False)
+
+import os
+PORT = int(os.getenv('server_port'))
+demo.queue().launch(inline=False, share=False, debug=True, server_name='0.0.0.0', server_port=PORT)
---
+++ worker_runpod.py
@@ -0,0 +1,70 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+import json, os, requests
+import runpod
+
+discord_token = os.getenv('com_camenduru_discord_token')
+web_uri = os.getenv('com_camenduru_web_uri')
+web_token = os.getenv('com_camenduru_web_token')
+
+pipe = AutoPipelineForText2Image.from_pretrained(
+    "/content/model",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    requires_safety_checker=False).to("cuda:0")
+
+def closestNumber(n, m):
+    q = int(n / m)
+    n1 = m * q
+    if (n * m) > 0:
+        n2 = m * (q + 1)
+    else:
+        n2 = m * (q - 1)
+    if abs(n - n1) < abs(n - n2):
+        return n1
+    return n2
+
+def generate(input):
+    values = input["input"]
+    width = closestNumber(values['width'], 8)
+    height = closestNumber(values['height'], 8)
+    images = pipe(values['prompt'], negative_prompt=values['negative_prompt'], num_inference_steps=25, guidance_scale=7.5, width=width, height=height)
+    result = f"/content/{input['id']}.png"
+    images.images[0].save(result)
+    
+    response = None
+    try:
+        source_id = values['source_id']
+        del values['source_id']
+        source_channel = values['source_channel']     
+        del values['source_channel']
+        job_id = values['job_id']
+        del values['job_id']
+        default_filename = os.path.basename(result)
+        files = {default_filename: open(result, "rb").read()}
+        payload = {"content": f"{json.dumps(values)} <@{source_id}>"}
+        response = requests.post(
+            f"https://discord.com/api/v9/channels/{source_channel}/messages",
+            data=payload,
+            headers={"authorization": f"Bot {discord_token}"},
+            files=files
+        )
+        response.raise_for_status()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        if os.path.exists(result):
+            os.remove(result)
+
+    if response and response.status_code == 200:
+        try:
+            payload = {"jobId": job_id, "result": response.json()['attachments'][0]['url']}
+            requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        finally:
+            return {"result": response.json()['attachments'][0]['url']}
+    else:
+        return {"result": "ERROR"}
+
+runpod.serverless.start({"handler": generate})
